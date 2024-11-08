1de1f41f58cd370433620c4d8fbb3cf6e183a05d
Amanda <e0969910@u.nus.edu>
2024-11-08 21:18:43 +0800

    Please check. I hope i didnt fuck up git

--- src/train_utils.py
+++ src/train_utils.py
@@ -12,7 +12,9 @@ def schema_text(node_props, rels):
   
 def get_system_message(schema_text):
     return f"""
-        You are an assistant with an ability to generate Cypher queries.
+        You are a financial assistant with an ability to generate Cypher queries.
+        You have a good understanding of how companies interact and how global events can affect companies.
+        However, if any assumption is made based off global events, you will inform the user that this information is not found in the database
         Task: Generate Cypher queries to query a Neo4j graph database based on the provided schema definition.
         Instructions:
         Use only the provided relationship types.
