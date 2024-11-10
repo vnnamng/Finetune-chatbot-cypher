22562570be3bb34d2a8c8f5433260c6a800931ec
Amanda <e0969910@u.nus.edu>
2024-11-11 01:15:56 +0800

    Changes to cypher

--- src/train_cypher.py
+++ src/train_cypher.py
@@ -18,13 +18,13 @@ MATCH (i: Industry {name: 'Industrial Machinery'}) <- [w:IS_INVOLVED_IN] - (c : 
 2. Find all sibling companies of Tesla, Inc with ticker 'TSLA'
 
 ```
-MATCH (c:Company {ticker: 'TSLA'}) <-[:SUBSIDIARY_OF]- (parent) -[:SUBSIDIARY_OF]-> (sibling) RETURN sibling
+MATCH (c:Company {ticker: 'TSLA'}) -[:SUBSIDIARY_OF] -> (parent) <- [:SUBSIDIARY_OF]-> (sibling) RETURN sibling
 ```
 
 3. Find all the subsidaries of Apple
 
 ```
-MATCH (c:Company ) - [s:SUBSIDIARY_OF] -> (o:Company) WHERE ANY (name IN c.names WHERE name = 'Apple') RETURN o
+MATCH (c:Company ) <- [s:SUBSIDIARY_OF] - (o:Company) WHERE ANY (name IN c.names WHERE name = 'Apple') RETURN o
 ```
 
 4. List all countries with a corporate tax rate higher than 20%
@@ -45,16 +45,16 @@ MATCH (r:Region) <-[:IS_IN]- (c:Country) WHERE c.gdp > 1000 RETURN r
 MATCH (c:Company) -[:HEADQUARTERS_IN]-> (cn:Country ) - [:IS_IN] -> (r:Region)  WHERE r.name CONTAINS 'Asia' RETURN c.names
 ```
 
-7. What are the names of companies Operating in Europe?
+7. What countries does Adobe operate in and what is their headcount for that country
 
 ```
-MATCH (c:Company) -[:OPERATES_IN]-> (cn:Country ) - [:IS_IN] -> (r:Region)  WHERE r.name CONTAINS 'Europe' RETURN c.names
+MATCH (c:Company) - [r: OPERATES_IN] - (cn:Country) WHERE ANY (name IN c.names WHERE name = 'Adobe') RETURN cn, r
 ```
 
-8 Which companies have been founded before 2000 and operate in the United States?
+8. What countries does Apple operate in and what is the corporate tax rate in that country?
 
 ```
-MATCH (c:Company)-[:OPERATES_IN]->(cn :Country) WHERE ANY (aliase IN cn.aliases WHERE aliase = 'United States') AND c.founded_year < '2020' RETURN c
+MATCH (c:Company) - [r: OPERATES_IN] - (cn:Country) WHERE ANY (name IN c.names WHERE name = 'Apple') RETURN cn.name, cn.corporate_tax_rate
 ```
 
 9. What companies are in Retail REITs and operates in India
@@ -93,10 +93,10 @@ MATCH (cn:Country) <-[:OPERATES_IN]- (c:Company) WITH c, count(DISTINCT cn) AS c
 MATCH (c:Company) -[:IS_INVOLVED_IN]-> (I:Industry) WHERE I.description CONTAINS 'Software' RETURN DISTINCT c
 ```
 
-15. List all industries with companies that have no subsidiaries
+15. what are all the companies that will affect APPLE what are their relationships?
 
 ```
-MATCH (i:Industry) <-[:IS_INVOLVED_IN]- (c:Company) WHERE NOT (c)-[:SUBSIDIARY_OF]->() RETURN DISTINCT i
+MATCH (c:Company) -[r:COMPETES_WITH | SUPPLIES_TO | SUBSIDIARY_OF]-> (a:Company) WHERE ANY(name IN a.names WHERE name = 'Apple') RETURN c, r
 ```
 
 16. How many companies operate in the 'Telecommunication Services' sector?
@@ -117,29 +117,10 @@ MATCH (:Country {name: 'Japan'})<-[:HEADQUARTERS_IN]-(c:Company)-[:COMPETES_WITH
 MATCH (i:Industry)<-[:IS_INVOLVED_IN]-(c:Company) RETURN i, COUNT(c) AS numCompanies ORDER BY numCompanies DESC LIMIT 5
 ```
 
-19. What companies have operations in more than one country?
+19. What is the parent company of Booking Holdings Inc
 
 ```
-MATCH (c:Company)-[:OPERATES_IN]->(cy:Country) WITH c, COUNT(cy) AS countries WHERE countries > 1 RETURN c
-```
-
-20. what are all the companies that will affect APPLE what are their relationships?
-
-```
-MATCH (c:Company) -[r:COMPETES_WITH | SUPPLIES_TO | SUBSIDIARY_OF]-> (a:Company) WHERE ANY(name IN a.names WHERE name = 'Apple') RETURN c, r
-```
-21. What countries does Apple operate in and what is the corporate tax rate in that country?
-
-```
-MATCH (c:Company) - [r: OPERATES_IN] - (cn:Country) WHERE ANY (name IN c.names WHERE name = 'Apple') RETURN cn.name, cn.corporate_tax_rate
-```
-
-22. What countries does Adobe operate in and what is their headcount for that country
-
-```
-MATCH (c:Company) - [r: OPERATES_IN] - (cn:Country) WHERE ANY (name IN c.names WHERE name = 'Adobe') RETURN cn, r
-```
-
+MATCH (c:Company) -[r:SUBSIDIARY_OF]-> (a:Company) WHERE ANY(name IN c.names WHERE name = 'Booking Holdings Inc') RETURN a
 """
 
 nodes, relationships, relationships_direction = get_schema(host, user, password)
